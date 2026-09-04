60647bcc6e6609abfd6029d5c4cf5e76314abcd0
Arcadio Nseng <arcadio.dev@gmail.com>
2026-09-04 16:48:37 +0100

    fix: check user permission for import operation
    fix: style import button properly

--- src/payload/collections/attendants/endpoint-handlers.ts
+++ src/payload/collections/attendants/endpoint-handlers.ts
@@ -16,6 +16,12 @@ import {
 export const importHandler: PayloadHandler = async (req) => {
   await addDataAndFileToRequest(req)
 
+  if (!req.user) {
+    throw new APIError('No tienes permiso para realizar esta acción', 401)
+  } else if (req.user.role !== 'admin') {
+    throw new APIError('No tienes permiso para realizar esta acción', 403)
+  }
+
   if (!req.file) {
     return Response.json({ error: 'No se ha proporcionado un archivo' }, { status: 400 })
   }
