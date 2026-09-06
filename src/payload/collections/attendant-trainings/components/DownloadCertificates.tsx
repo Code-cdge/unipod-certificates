670e726cf6409122502ffb7292045be2966562d8
Arcadio Nseng <arcadio.dev@gmail.com>
2026-09-06 13:50:56 +0100

    fix: show loading overlay while downloading certificates

--- src/payload/collections/attendant-trainings/components/DownloadCertificates.tsx
+++ src/payload/collections/attendant-trainings/components/DownloadCertificates.tsx
@@ -1,24 +1,46 @@
 'use client'
 
-import { Button, useListQuery } from '@payloadcms/ui'
+import { Button, LoadingOverlay, toast, useListQuery } from '@payloadcms/ui'
+import { useState } from 'react'
 
 export default function downloadCertificates() {
-
   const { collectionSlug, data } = useListQuery()
+  const [loading, setLoading] = useState<boolean>(false)
 
-
-  const handleClick = () => {
+  const handleClick = async () => {
     const ids = data?.docs.map((doc) => doc.id).join(',')
     if (ids) {
-      window.location.href = `/api/${collectionSlug}/download-certificates?ids=${ids}`
+      setLoading(true)
+      const response = await fetch(`/api/${collectionSlug}/download-certificates?ids=${ids}`, {
+        method: 'GET',
+        credentials: 'include',
+      })
+
+      if (!response.ok) {
+        console.error(await response.json())
+        return toast.error('Se ha producido un error al descargar los certificados')
+      }
+
+      const blob = await response.blob()
+      const blobUrl = window.URL.createObjectURL(blob)
+      const link = document.createElement('a')
+      link.href = blobUrl
+      link.download = `certificados-${data?.page}-${data?.totalPages}.zip`
+      document.body.appendChild(link)
+      link.click()
+      link.remove()
+      window.URL.revokeObjectURL(blobUrl)
+      setLoading(false)
     }
   }
 
   return (
     <>
+      <LoadingOverlay show={loading} loadingText={'Descargando certificados...'} />
       <Button
         icon={['chevron']}
         buttonStyle={'subtle'}
+        disabled={loading}
         onClick={handleClick}
       >
         Descargar certificados
